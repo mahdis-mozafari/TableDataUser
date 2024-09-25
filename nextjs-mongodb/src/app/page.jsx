36155005e77510ab003ba92ceb52ecb48d37fe33
mahdismozafari <mahdis.bano3@gmail.com>
2024-09-25 23:05:50 +0330

    render the component in this file

--- nextjs-mongodb/src/app/page.jsx
+++ nextjs-mongodb/src/app/page.jsx
@@ -1,7 +1,21 @@
+
 import ModalEport from "@/component/ExportModal";
+import ModalImport from "@/component/ImportModal";
+import BasicTable from "@/component/Table";
+import { Stack } from "@mui/material";
+
 
 export default function Home() {
-  return (
-  <ModalEport/>
+ 
+
+return (
+<>
+<Stack spacing={2} direction="row" justifyContent={'space-around'} >
+   <ModalImport/>
+   <ModalEport/>
+            </Stack>
+   
+    <BasicTable/>
+</>
   );
 }
